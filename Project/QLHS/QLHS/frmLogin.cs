131f0d469ae0669f2703cb0b4b82471da5d7cf49
THANH-CONG\owlbo <16520131@gm.uit.edu.vn>
2018-04-10 01:39:01 +0700

    edit database + form login
    1. Đã chỉnh sửa lại database hoàn chỉnh
    2. Đã code xong back-end login form với tài khoản và mật khẩu ở trong database
    3. Đã thiết lập kết nối với server

---
+++ Project/QLHS/QLHS/frmLogin.cs
@@ -0,0 +1,75 @@
+﻿using QLHS.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLHS
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangnhap_Click(object sender, EventArgs e)
+        {
+            string userName = txtTen.Text;
+            string passWord = txtMatkhau.Text;
+            if(_login(userName, passWord))
+            {
+                frmMain main = new frmMain();
+                this.Hide();
+                main.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Thông tin sai!");
+            }
+
+        }
+
+        bool _login(string userName, string passWord)
+        {
+            string query = "EXEC dbo.USP_User_Login @userName , @passWord";
+
+            DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] { userName, passWord });
+
+            return result.Rows.Count > 0;
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát khỏi ứng dụng?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
